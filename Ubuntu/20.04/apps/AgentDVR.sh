3ede169c02eded403e98f3d8ba88befb08e99de5
jferrada <juan.ferrada@gmail.com>
2023-11-05 00:43:50 -0300

    Agregando soporte para usuario root a AgentDVR

--- Ubuntu/20.04/apps/AgentDVR.sh
+++ Ubuntu/20.04/apps/AgentDVR.sh
@@ -4,12 +4,14 @@ IFS=$'\n\t'
 
 if [ -n "$SUDO_USER" ]; then
 	user="$SUDO_USER"
+	dir="/home/$user/AgentDVR"
 else
 	user='root'
+	dir='/root/AgentDVR'
 fi
 
 apt install -y curl
 
-if [ ! -d "/home/$user/AgentDVR" ]; then
+if [ ! -d "$dir" ]; then
 	su -l "$user" -c "$(curl -fsSL https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/v2/install.sh)"
 fi
